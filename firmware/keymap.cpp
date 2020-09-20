7436234d36d7ec6de9ad4cece80f0b2b4083b89a
Rhylx <redbull11570@gmail.com>
2020-09-20 17:19:35 +0200

    new layout

--- firmware/keymap.cpp
+++ firmware/keymap.cpp
@@ -6,23 +6,23 @@
  |    | Q  | W  | e  | R  | T  | Y  | U  | I  | O  | P  | L2 |
  | TAB| q  | w  | e  | r  | t  | y  | u  | i  | o  | p  | L2 |
  =============================================================
- |    | A  | S  | D  | F  | G  | ;  | H  | J  | K  | L  | ^  |
- | L1 | a  | s  | d  | f  | g  | .  | h  | j  | k  | l  | |  |
+ | ^  | A  | S  | D  | F  | G  | :  | H  | J  | K  | L  | ^  |
+ | |  | a  | s  | d  | f  | g  | .  | h  | j  | k  | l  | |  |
  =============================================================
  | ^  | Z  | X  | C  | V  | B  | N  | M  | `  | ¨  | ?  | ç  |
  | |  | z  | x  | c  | v  | b  | n  | m  | ´  | ^  | !  | @  |
  =============================================================
- |    |    |    |    |    |         |    |    |    |    |    |
- |    |    |CTRL| ENT| ESC|  SPACE  | <- | SUP| ALT|    |    |
+ |    |    |    |    |    |    |    |    |    |    |    |    |
+ |    |    |CTRL| SUP| ESC| L1 ||_| | <- | ENT| ALT|    |    |
  =============================================================
 */
 
 std::array<std::array<Key, MATRIX_COLS>, MATRIX_ROWS> matrix =
     {{
-        {KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,   KC_T,   KC_Y,   KC_U,    KC_I,     KC_O,   KC_P,     LAYER_2,},
-        {LAYER_1, KC_A,    KC_S,    KC_D,    KC_F,   KC_G,   KC_DOT, KC_H,    KC_J,     KC_K,   KC_L,     KC_RSFT,},
-        {KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,   KC_B,   KC_N,   KC_M,    KC_SLASH, KC_GRV,	KC_MINS,  KC_EQUAL},
-        {_______, _______, KC_LCTL, KC_ENT, KC_ESC, KC_SPC, KC_SPC, KC_BSPC, KC_LGUI,   KC_LALT ,_______, _______}
+        {KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,   KC_T,    KC_Y,   KC_U,    KC_I,     KC_O,   KC_P,     LAYER_2,},
+        {KC_LSFT, KC_A,    KC_S,    KC_D,    KC_F,   KC_G,    KC_DOT, KC_H,    KC_J,     KC_K,   KC_L,     KC_RSFT,},
+        {KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,   KC_B,    KC_N,   KC_M,    KC_SLASH, KC_GRV, KC_MINS,  KC_EQUAL},
+        {_______, _______, KC_LCTL, KC_LGUI, KC_ESC, LAYER_1, KC_SPC, KC_BSPC, KC_ENT,   KC_LALT, _______, _______}
     }};
 
 
@@ -33,49 +33,49 @@ void setupKeymap() {
 LAYOUT 1 :
   ____ ____ ____ ____ ____ ____ ____ ____ ____ ____ ____ ____
  |    |    |    |    |    |    |    |    |    |    |    |    |
- | TAB|  # |  > |  - | 7  | 8  | 9  |LEFT|DOWN| UP |RGHT| L2 |
+ | TAB|  # |  > |  - | {  | +  | '  | 7  | 8  | 9  |    | L2 |
  =============================================================
- |    |    |    |    |    |    |    |    |    |    |    |  ^ |
- | L1 |  § |  / |  $ | 4  | 5  | 6  | [  |  ( |  { |  | |  | |
+ | ^  |    |    |    |    |    |    |    |    |    |    |  ^ |
+ | |  |  § |  / |  $ | (  | [  | ,  | 4  | 5  | 6  |  | |  | |
  =============================================================
- |  ^ |    |    |    |    |    |    |    |    |    |    |    |
- |  | |    |    | 0  | 1  | 2  | 3  | ,  |  ' | +  | &  |    |
+ | ^  |    |    |    |    |    |    |    |    |    |    |    |
+ | |  |LEFT|DOWN| UP |RGHT|    | 0  | 1  | 2  | 3  | &  |    |
  =============================================================
- |    |    |    |    |    |         |    |    |    |    |    |
- |    |    |CTRL| ENT| ESC|  SPACE  | <- | SUP| ALT|    |    |
+ |    |    |    |    |    |    |    |    |    |    |    |    |
+ |    |    |CTRL| SUP| ESC| L1 ||_| | <- | ENT| ALT|    |    |
  =============================================================
 */
 
     uint32_t layer1[MATRIX_ROWS][MATRIX_COLS] =
         KEYMAP(
-    KC_TAB,  KC_PMNS, KC_SCOLON, KC_KP_3, KC_7,   KC_8,   KC_9,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT,   LAYER_2,
-    LAYER_1, KC_KP_1, KC_KP_2,   KC_KP_8, KC_4,   KC_5,   KC_6,   KC_LBRC, KC_F24,  KC_KP_5, KC_KP_DOT, KC_RSFT,
-    KC_LSFT, _______, _______,   KC_0,    KC_1,   KC_2,   KC_3,   KC_COMM, KC_SLCK, KC_END,  KC_F19,    _______,
-    _______, _______, KC_LCTL,   KC_ENT, KC_ESC, KC_SPC, KC_SPC, KC_BSPC, KC_LGUI,  KC_LALT, _______,   _______ );
+    KC_TAB,  KC_PMNS, KC_SCOLON, KC_KP_3, KC_KP_5, KC_END,   KC_SLCK, KC_7,    KC_8,   KC_9,    _______,   LAYER_2,
+    KC_LSFT, KC_KP_1, KC_KP_2,   KC_KP_8, KC_F24,  KC_LBRC,  KC_COMM, KC_4,    KC_5,   KC_6,    KC_KP_DOT, KC_RSFT,
+    KC_LSFT, KC_LEFT, KC_DOWN,   KC_UP,   KC_RGHT, _______, KC_0,    KC_1,    KC_2,   KC_3,    KC_F19,    _______,
+    _______, _______, KC_LCTL,   KC_LGUI, KC_ESC,  LAYER_1,  KC_SPC,  KC_BSPC, KC_ENT, KC_LALT, _______,   _______ );
 
 /*
  LAYOUT 2:
   ____ ____ ____ ____ ____ ____ ____ ____ ____ ____ ____ ____
  |    |    |    |    |    |    |    |    |    |    |    |    |
- | TAB|  % |  < |  _ | 7  | 8  | 9  |LEFT|DOWN| UP |RGHT| L2 |
+ | TAB|  % |  < |  _ | }  |    | "  | *  |    |    |    | L2 |
  =============================================================
- |    |    |    |    |    |    |    |    |    |    |    |  ^ |
- | L1 |  ~ |  \ |  $ | 4  | 5  | 6  | ]  |  ) |  } |  | |  | |
+ | ^  |    |    |    |    |    |    |    |    |    |    |  ^ |
+ | |  |  ~ |  \ |  $ | )  | ]  | ;  | =  |    |    |  | |  | |
  =============================================================
- |  ^ |    |    |    |    |    |    |    |    |    |    |    |
- |  | |  * | =  | 0  | 1  | 2  | 3  | :  |  " |    |    |    |
+ | ^  |    |    |    |    |    |    |    |    |    |    |    |
+ | |  |    |    |    |    |    |    |    |    |    |    |    |
  =============================================================
  |    |    |    |    |    |         |    |    |    |    |    |
- |    |    |CTRL| ENT| ESC|  SPACE  | <- | SUP| ALT|    |    |
+ |    |    |CTRL| SUP| ESC|  SPACE  | <- | ENT| ALT|    |    |
  =============================================================
 */
 
     uint32_t layer2[MATRIX_ROWS][MATRIX_COLS] =
         KEYMAP(
-    KC_TAB,  KC_PSLS, KC_PPLS, KC_KP_9, KC_7,   KC_8,   KC_9,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT,   LAYER_2,
-    LAYER_1, KC_KP_7, KC_BSLS, KC_KP_8, KC_4,   KC_5,   KC_6,   KC_RBRC, KC_KP_0, KC_KP_6, KC_KP_DOT, KC_RSFT,
-    KC_LSFT, KC_PAST, KC_KP_4, KC_0,    KC_1,   KC_2,   KC_3,   KC_PEQL, KC_QUOT, _______, _______,   _______ ,
-    _______, _______, KC_LCTL, KC_ENT,  KC_ESC, KC_SPC, KC_SPC, KC_BSPC, KC_LGUI, KC_LALT, _______,   _______ );
+    KC_TAB,  KC_PSLS, KC_PPLS, KC_KP_9, KC_KP_DOT, _______,  KC_QUOT, KC_PAST, _______, _______, _______,   LAYER_2,
+    KC_LSFT, KC_KP_7, KC_BSLS, KC_KP_8, KC_KP_0,   KC_RBRC, KC_PEQL, KC_KP_4, _______, _______, KC_KP_DOT, KC_RSFT,
+    KC_LSFT, _______, _______, _______, _______,   _______, _______, _______, _______, _______, _______,   _______ ,
+    _______, _______, KC_LCTL, KC_LGUI, KC_ESC,    KC_SPC,  KC_SPC,  KC_BSPC, KC_ENT,  KC_LALT, _______,   _______ );
 
     /*
      * add the other layers
